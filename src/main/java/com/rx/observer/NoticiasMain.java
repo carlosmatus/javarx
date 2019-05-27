7a78a6be7ed71472a58dcac2db40d7b68526526c
carlos <carlos_matus@hotmail.com>
2019-05-27 15:19:23 -0500

    Errores corregidos de observaciones y probado con mas datos

--- src/main/java/com/rx/observer/NoticiasMain.java
+++ src/main/java/com/rx/observer/NoticiasMain.java
@@ -14,10 +14,15 @@ public class NoticiasMain {
          News polNews = new PoliticsNews("Noticia Politica ");
         News sportsNews = new SportsNews("Noticia Deportes");
         News showBNews = new ShowBNews("Noticia Espectaculos");
+        News sportsNews1 = new SportsNews("Noticia Deportes 2");
+        News polNews2 = new PoliticsNews("Noticia Politica 2");
+
         ArrayList<News> newsPool = new ArrayList<>();
         newsPool.add(polNews);
         newsPool.add(showBNews);
         newsPool.add(sportsNews);
+        newsPool.add(sportsNews1);
+        newsPool.add(polNews2);
 
         Observable obs = Observable.from(newsPool);
 
@@ -27,7 +32,7 @@ public class NoticiasMain {
         Subscriber polSubscriber = getSubscriber(NewsType.POLITICS);
         obs.subscribe(polSubscriber);
         Subscriber deportSubscriber = getSubscriber(NewsType.SPORTS);
-        obs.subscribe(deportSubscriber);
+        obs.subscribe(getSubscriber(NewsType.SPORTS));
         obs.subscribe(getSubscriber(NewsType.SHOWBUSINESS,NewsType.POLITICS));
         obs.subscribe(getSubscriber(NewsType.POLITICS,NewsType.SPORTS,NewsType.SHOWBUSINESS));
     }
@@ -48,12 +53,21 @@ public class NoticiasMain {
 
             @Override
             public void onNext(News news) {
-                List<NewsType> lst = Arrays.asList(nType);
-                if(lst.stream().filter(type-> type.compareTo(NewsType.POLITICS)==0 ).findAny().orElse(null)!=null && news.getNewsType().compareTo(NewsType.SHOWBUSINESS)==0)
-                    this.unsubscribe();
-                if(lst.stream().filter(type-> type==news.getNewsType()).findAny().orElse(null)!=null)
-                System.out.println("Tipo Subscriber "+nType+"  Tipo de noticias "+news);
 
+                if(Arrays.toString(nType).indexOf(NewsType.POLITICS.toString())>-1 && news.getNewsType().compareTo(NewsType.SHOWBUSINESS)==0)
+                {
+                    System.out.println("Unsubscribe tipo suscriptor "+Arrays.toString(nType) +" al recibir Noticia :"+news.getNewsType());
+                    this.unsubscribe();
+                    return;
+                }
+
+                if(Arrays.toString(nType).indexOf(news.getNewsType().toString())>-1) {
+
+                    System.out.println("Tipo Subscriber " + Arrays.toString(nType) + "  Tipo de noticias " + news);
+
+                }else{
+                    System.out.println("No esta suscrito a este tipo de noticias, Tipo Suscriptor:"+Arrays.toString(nType)+"Tipo Noticias Recibido : "+news.getNews().toString());
+                }
             }
         };
     }
